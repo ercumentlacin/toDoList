64559af65b706c61c5841f5f8dd98bf631c11bf3
ercüment <41874858+ercumentlacin@users.noreply.github.com>
2020-12-05 12:55:33 +0300

    Update script.js

--- script.js
+++ script.js
@@ -80,10 +80,10 @@ document.getElementById("id_of_textbox")
         document.getElementById("id_of_button").click();
     }
 });
-document.getElementById("new-item-enter")
-    .addEventListener("keyup", function(event) {
-    event.preventDefault();
-    if (event.keyCode === 13) {
-        document.getElementByClass("enter").click();
-    }
-});
+// document.getElementById("new-item-enter")
+//     .addEventListener("keyup", function(event) {
+//     event.preventDefault();
+//     if (event.keyCode === 13) {
+//         document.getElementByClass("enter").click();
+//     }
+// });
